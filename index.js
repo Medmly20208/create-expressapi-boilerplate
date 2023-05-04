9b9d7523709ce917c69b41a9f6942690628bac74
Medmly20208 <moulaymohamed856@gmail.com>
2023-05-04 19:21:58 +0100

    update index file

--- index.js
+++ index.js
@@ -20,31 +20,22 @@ fs.mkdirSync(projectDir, { recursive: true });
 const templateDir = path.resolve(__dirname, "./");
 fs.cpSync(templateDir, projectDir, { recursive: true });
 
-// It is good practice to have dotfiles stored in the
-// template without the dot (so they do not get picked
-// up by the starter template repository). We can rename
-// the dotfiles after we have copied them over to the
-// new project directory.
-fs.renameSync(
-  path.join(projectDir, "gitignore"),
-  path.join(projectDir, ".gitignore")
-);
-
 const projectPackageJson = require(path.join(projectDir, "package.json"));
 
 // Update the project's package.json with the new project name
 projectPackageJson.name = projectName;
+projectPackageJson.description = "";
+projectPackageJson.main = "";
+projectPackageJson.bin = {};
+projectPackageJson.files = [];
 
 fs.writeFileSync(
   path.join(projectDir, "package.json"),
   JSON.stringify(projectPackageJson, null, 2)
 );
-
-// Run `npm install` in the project directory to install
-// the dependencies. We are using a third-party library
-// called `cross-spawn` for cross-platform support.
-// (Node has issues spawning child processes in Windows).
-spawn.sync("npm", ["install"], { stdio: "inherit" });
-
+//delete this file
+fs.unlink(`${projectDir}/index.js`, (err) => {
+  if (err) throw err;
+});
 console.log("Success! Your new project is ready.");
 console.log(`Created ${projectName} at ${projectDir}`);
